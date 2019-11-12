4b7200433373d6eee8bc889409d5ac9bd475c40b
Ashish <ashishteddy2@gmail.com>
2019-11-12 17:34:00 +0530

    done with edge cases

--- bin/test.dart
+++ bin/test.dart
@@ -13,4 +13,15 @@ void main() {
     expect(bubbleSort(list), equals([1,2,4,5,8]));
   },
   );
+
+  test("Bubble Sort", () {
+    var list = [5, 1, 2, 4, 8];
+    for (var x in list) {
+      if (x == null || !(x is int)) {
+        throw ArgumentError;
+      }
+    }
+    expect(bubbleSort(list), equals([1, 2, 4, 5, 8]));
+  },
+  );
 }
